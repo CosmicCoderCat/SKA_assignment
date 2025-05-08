2d766f4f127d6fdf6bd1c52865a10dc6da1372fc
CosmicCoderCat <133282324+CosmicCoderCat@users.noreply.github.com>
2025-05-08 13:58:43 +0100

    Added analysing.py to __init__.py.

--- src/SKA_assignment/__init__.py
+++ src/SKA_assignment/__init__.py
@@ -1,6 +1,7 @@
 from .data_handler import DataHandler
 from .imaging import get_dirty_image, get_psf, get_beam, deconvolve_image
 from .plotting import plot_dirty_clean, plot_amplitude_vs_time
+from .analysing import monitor_data_quality
 
 __all__ = [
     "DataHandler",
@@ -10,4 +11,5 @@ __all__ = [
     "deconvolve_image",
     "plot_dirty_clean",
     "plot_amplitude_vs_time",
+    "monitor_data_quality",
 ]
